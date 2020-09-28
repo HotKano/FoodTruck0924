4814164f3add1d5eb9f649478168c0e24c9099ba
김종우 <44638291+HotKano@users.noreply.github.com>
2020-09-28 17:47:32 +0900

    김종우
    마커 여러개 띄우는 것 테스트

--- components/Main.tsx
+++ components/Main.tsx
@@ -1,16 +1,14 @@
-import React, { } from 'react';
+import React, { useEffect, useState } from 'react';
 import 'react-native-gesture-handler';
 import {
     View,
     Button,
     Text,
 } from 'react-native';
-import { createStackNavigator } from '@react-navigation/stack';
 import Geolocation from '@react-native-community/geolocation'
 import MapView, { Marker } from 'react-native-maps'
 import styled from 'styled-components/native';
 import SplashScreen from 'react-native-splash-screen';
-import { initialWindowMetrics } from 'react-native-safe-area-context';
 
 const Container = styled.View`
   flex: 1;
@@ -39,8 +37,30 @@ const testData = [
     }
 ]
 
-export class HomeScreen extends React.Component {
+const mapTestData = [
+    {
+        latitude: 34.7724303,
+        longitude: 127.6985649
+    },
+    {
+        latitude: 34.7736233,
+        longitude: 127.7015145
+    },
+    {
+        latitude: 34.7734318,
+        longitude: 127.7019923
+    },
+    {
+        latitude: 34.7708991,
+        longitude: 127.7038201
+    },
+    {
+        latitude: 34.771274,
+        longitude: 127.7021169
+    }
+]
 
+export class HomeScreen extends React.Component {
     state = {
         title: "",
         poster: "",
@@ -62,12 +82,9 @@ export class HomeScreen extends React.Component {
         })
     }
 
-
     render() {
 
         SplashScreen.hide();
-
-        const { navigation } = this.props;
         const { user, boy } = this.props.route.params ?? 'defaultdata'
 
         return (
@@ -84,94 +101,77 @@ export class HomeScreen extends React.Component {
     }
 }
 
-export class NotificationsScreen extends React.Component {
-    state = {
-        data: {
-            lat: 0,
-            lon: 0,
-            acc: 0,
-        },
-    }
+interface ILocation {
+    latitude: number;
+    longitude: number;
+}
 
-    componentDidMount() {
-        Geolocation.getCurrentPosition(
-            (position) => {
-                this.setState({
-                    data: {
-                        lat: position.coords.latitude,
-                        lon: position.coords.longitude,
-                        acc: position.coords.accuracy
+const TrackUserLocation = () => {
+    const [location, setLocation] = useState<ILocation | undefined>(undefined);
+
+    useEffect(() => {
+        Geolocation.getCurrentPosition
+            (
+                position => {
+                    const { latitude, longitude } = position.coords;
+                    setLocation({
+                        latitude,
+                        longitude,
+                    });
+                },
+                error => {
+                    console.log(error);
+                },
+                {
+                    enableHighAccuracy: false,
+                    timeout: 15000,
+                    maximumAge: 10000
+                },
+            );
+    }, []);
+
+    return (
+        <Container>
+            { location && (
+                <MapView
+                    style={{ flex: 1 }}
+                    initialRegion={{
+                        // location.latitude
+                        latitude: 34.7724303,
+                        // location.longitude
+                        longitude: 127.6985649,
+                        latitudeDelta: 0.005,
+                        longitudeDelta: 0.005,
+                    }}>
+                    {
+                        mapTestData.map((data, i) =>
+                            (
+                                <Marker
+                                    key={i}
+                                    coordinate={{
+                                        latitude: data.latitude,
+                                        longitude: data.longitude,
+                                    }}
+                                />
+                            )
+                        )
                     }
-                })
-            },
-            (error) => {
-                // See error code charts below.
-                console.log(`${error.code, error.message} location error`);
-            },
-            {
-                enableHighAccuracy: true,
-                timeout: 20000,
-                maximumAge: 0
-            }
-        );
-    }
+                </MapView>
+            )}
+        </Container>
+    );
+};
 
+export class NotificationsScreen extends React.Component {
     render() {
-        const { navigation } = this.props;
         SplashScreen.hide();
-
         return (
             // <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
             //     <Button onPress={() => navigation.goBack()} title="Go back home" />
             // </View>
             <Container>
-                { <MapView
-                    // key 옵션이 없으면 맵이 마커로 이동하지 않음..
-                    key={(this.state.data.lat + this.state.data.lon+this.state.data.acc).toString()}
-                    style={{ flex: 1, }}
-                    initialRegion={{
-                        latitude: this.state.data.lat,
-                        longitude: this.state.data.lon,
-                        latitudeDelta: 0.005,
-                        longitudeDelta: 0.005,
-                    }}
-                >
-                    {
-                        <Marker
-                            coordinate={
-                                {
-                                    latitude: this.state.data.lat,
-                                    longitude: this.state.data.lon
-                                }
-                            }
-                            title="this is a marker"
-                            description="this is a marker example"
-                        />
-                    }
-
-                </MapView>}
+                <TrackUserLocation />
             </Container>
         );
     }
 }
-
-const Stack = createStackNavigator();
-
-// export class MainScreen extends React.Component {
-//     render() {
-//         const { navigation } = this.props;
-//         const { nickname } = this.props.route.params;
-
-//         console.log(`${nickname} wowwwwwwwwwwwwwwwwwww`);
-//         return (
-//                 <Stack.Navigator>
-//                     <Stack.Screen
-//                         name="Home"
-//                         component={HomeScreen}
-//                         initialParams={{ user: `${nickname}`, }}
-//                     />
-//                     <Stack.Screen name="Notifications" component={NotificationsScreen} />
-//                 </Stack.Navigator>
-//         )
-//     }
-// }
